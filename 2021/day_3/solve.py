6e1d1ea17b37f7552f977b1ae68ab01cfc7801fd
Istvan Csanyi <istvan.csanyi@onlogic.com>
2021-12-04 00:32:14 +0100

    [2021 Day3] Refactor to make it cleaner

--- 2021/day_3/solve.py
+++ 2021/day_3/solve.py
@@ -1,60 +1,56 @@
-# https://adventofcode.com/2021/day/2
+# https://adventofcode.com/2021/day/3
 from collections import defaultdict
 
 with open(r"input.txt") as file:
+    # Clean the input, this is an extra loop but we don't really care
     input = [x.strip() for x in file.readlines()]
 
+
 def find_common(numbers):
-    total_numbers = 0
     bit_sums = defaultdict(int)
-    # Add up the bits as integers, and take note of how many binary numbers we have
+    # Count the bits as integers
     for number in numbers:
-        total_numbers += 1
-        for idx, bit in enumerate(number.strip()):
+        for idx, bit in enumerate(number):
             bit_sums[idx] += int(bit)
 
-    most_common_list = []
-    least_common_list = []
-    half = total_numbers / 2
+    # Create the binary strings
+    # If the bit count is bigger than the half of the total numbers that will be the common one
+    most_common = least_common = ''
+    half = len(numbers) / 2
     for bit in bit_sums.values():
-        if bit >= half:
-            most_common_list.append('1')
-            least_common_list.append('0')
-        else:
-            most_common_list.append('0')
-            least_common_list.append('1')
-    return most_common_list, least_common_list
+        most_common += '1' if bit >= half else '0'
+        # We could use Bitwise Not but this is faster
+        least_common += '0' if bit >= half else '1'
+    return most_common, least_common
 
-
-most_common_list, least_common_list = find_common(input)
-gamma_rate_binary = ''.join(most_common_list)
-epsilon_rate_binary = ''.join(least_common_list)
-print(f"Part 1 solution: {int(gamma_rate_binary, 2) * int(epsilon_rate_binary, 2)}")
+# Part 1
+most_common, least_common = find_common(input)
+print(f"Part 1 solution: {int(most_common, 2) * int(least_common, 2)}")
 
 # Part2
-def filter_numbers(bits, numbers, idx, get_most_common=True):
+def filter_numbers(bits, numbers, idx):
+
     if len(numbers) == 1:
-        return False, numbers
-    new_numbers = []
-    co2_bit = bits[idx]
-    for number in numbers:
-        if number[idx] == co2_bit:
-            new_numbers.append(number)
-    most, least = find_common(new_numbers)
-    return most if get_most_common else least, new_numbers
+        # Short circuit if only one numer is left, the puzzle rules defined this
+        return False, False, numbers
+
+    # Get the numbers where the bit at the given index matches
+    new_numbers = [number for number in numbers if number[idx] == bits[idx]]
+
+    # Get the commons for the new numbers
+    most_common, least_common = find_common(new_numbers)
+
+    return most_common, least_common, new_numbers
 
 
-# Set the defaults
-o2_valid_bits = most_common_list
-co2_valid_bits = least_common_list
-o2_valid_numbers = co2_valid_numbers = input
+# Set the defaults for Part 2 start
+o2_bits = most_common
+co2_bits = least_common
+o2_numbers = co2_numbers = input
 
-# Loop trough each number index
-for idx in range(len(most_common_list)):
-    # print(f"co2_valid_bits: {co2_valid_bits} co2_valid_numbers: {co2_valid_numbers} bit: {o2_valid_bits[idx]}")
-    o2_valid_bits, o2_valid_numbers = filter_numbers(o2_valid_bits, o2_valid_numbers, idx)
-    co2_valid_bits, co2_valid_numbers = filter_numbers(
-        co2_valid_bits, co2_valid_numbers, idx, get_most_common=False
-    )
+for idx in range(len(most_common)):
+    # Loop trough each bit index and filter down the valid numbers in each cycle
+    o2_bits, _, o2_numbers = filter_numbers(o2_bits, o2_numbers, idx)
+    _, co2_bits, co2_numbers = filter_numbers(co2_bits, co2_numbers, idx)
 
-print(f"Part 2 solution: {int(o2_valid_numbers[0], 2) * int(co2_valid_numbers[0], 2)}")
+print(f"Part 2 solution: {int(o2_numbers[0], 2) * int(co2_numbers[0], 2)}")
