0c492d389970287cc445527199f7e76601ae46b6
Istvan Csanyi <istvan.csanyi@logicsupply.com>
2022-12-01 22:10:46 +0100

    [2022-D1] Solve it

--- 2022/src/day1/solve.ts
+++ 2022/src/day1/solve.ts
@@ -3,17 +3,37 @@ import { getInput } from "../utils/input";
 export async function solve(dayNumber: string) {
   const input: string = getInput({
     dayNumber: dayNumber,
-    example: true,
+    example: false,
   });
   console.log(`---- Day ${dayNumber} ----`);
 
-  // Split the string to an array based on new lines
-  const inputArray = input.split("\n");
+  // Split the string to an array of integers based on new lines
+  const inputArray = input.split("\n").map((x) => (x === "" ? null : +x));
 
-  // Solve Part1
-  await part1(inputArray);
+  // Solve Part1&2
+  await allParts(inputArray);
 }
 
-async function part1(input: string[]) {
-  console.log(input);
+async function allParts(input: (number | null)[]) {
+  const totals: number[] = [];
+  let currentTotal = 0;
+  input.forEach((element) => {
+    if (element === null) {
+      totals.push(currentTotal);
+      currentTotal = 0;
+    } else {
+      currentTotal += element;
+    }
+  });
+
+  // Sort the total in descending order
+  const totalSorted = totals.sort((a, b) => a - b).reverse();
+
+  // First element is the highest value
+  console.log(`Part1: ${totalSorted[0]}`);
+
+  // Splice to the first highest elements than get `sum` with reduce
+  console.log(
+    `Part2: ${totalSorted.slice(0, 3).reduce((sum, x) => sum + x, 0)}`
+  );
 }
