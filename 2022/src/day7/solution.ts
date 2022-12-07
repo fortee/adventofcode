a0f7e7f2fd5a4618a2185699f8c4a319be5a5545
Istvan Csanyi <istvan.csanyi@onlogic.com>
2022-12-08 00:44:22 +0100

    [2022 Day7] Cleanup and more comments

--- 2022/src/day7/solution.ts
+++ 2022/src/day7/solution.ts
@@ -1,29 +1,5 @@
 import { performance } from 'perf_hooks';
 
-/**
- * Main function to trigger all functionality needed to solve the daily challenge
- * @param input - `raw` string content of the input file
- */
-export async function solve(input: string, dayNumber: string) {
-  const st = performance.now();
-  console.log(`---- Day ${dayNumber} ----`);
-
-  // Split the string to an array of items
-  const inputArray = input.split("\n");
-
-  // We can remove the first two lines
-  inputArray.splice(0, 2);
-
-  // Build the directory tree
-  createDirectoryTree(inputArray);
-
-
-  // Solve Part 1&2
-  await doit(inputArray);
-
-  console.log(`Done in ${((performance.now() - st) / 1000).toFixed(2)}s`);
-}
-
 // Type we will use to define directory objects
 type dir = {
   name: string,
@@ -53,11 +29,23 @@ let part1Solution = 0;
 let currentFreeSpace = 0;
 let part2Solution: number | undefined = undefined;
 
+
 /**
- * Solve the puzzle
- * @param input - The Puzzle input
+ * Main function to trigger all functionality needed to solve the daily challenge
+ * @param input - `raw` string content of the input file
  */
-async function doit(input: string[]): Promise<void> {
+export async function solve(input: string, dayNumber: string) {
+  const st = performance.now();
+  console.log(`---- Day ${dayNumber} ----`);
+
+  // Split the string to an array of items
+  const inputArray = input.split("\n");
+
+  // We can remove the first two lines
+  inputArray.splice(0, 2);
+
+  // Build the directory tree
+  createDirectoryTree(inputArray);
 
   // Once the directory tree is created the root directory should have it's size set correctly
   currentFreeSpace = totalSpace - rootDirectory.size;
@@ -69,6 +57,7 @@ async function doit(input: string[]): Promise<void> {
   console.log(`Part1: ${part1Solution}`);
   console.log(`Part2: ${part2Solution}`);
 
+  console.log(`Done in ${((performance.now() - st) / 1000).toFixed(2)}s`);
 }
 
 /**
@@ -80,13 +69,19 @@ function createDirectoryTree(input: string[]): void {
   // Manually set the root dir as the current starting dir
   let currentDirectory = rootDirectory;
 
-  // Keep track of the row index
   for (const line of input) {
+    // Loop trough the input
+
+    // Get the line data
     const lineData = line.split(" ");
+
     switch (lineData[0]) {
+      // Run different functionality depending on the first word 
       case "$":
+        // This is some kind of command
 
         if (lineData[1] == "cd" && lineData[2] == ".." && currentDirectory.parent !== undefined) {
+          // Move up one level
           // If the current directory has a parent directory move up to it
           if (currentDirectory.parent !== undefined) currentDirectory = currentDirectory.parent;
           break;
@@ -94,20 +89,22 @@ function createDirectoryTree(input: string[]): void {
 
         // Try to find the directory between the children of the current directory
         const targeDirectory = currentDirectory.children.find(d => d.name === lineData[2]);
+
         if (lineData[1] == "cd" && targeDirectory !== undefined) {
           // Move to the given directory if it already exists as child under the current directory
           currentDirectory = targeDirectory;
           break;
         }
 
+        // We don't need to worry about the rest of the commands
         break;
       case "dir":
-        // This is a directory
+        // The content of the current directory contains another directory
         createNewDirectory(lineData[1], currentDirectory);
         break;
       default:
-        // This is a file
-        addFilesSize(currentDirectory, +lineData[0]);
+        // The content of the current directory contains a file
+        updateDirectorySizeWithFileSize(currentDirectory, +lineData[0]);
         currentDirectory.files.push(line);
         break;
     }
@@ -120,36 +117,40 @@ function createDirectoryTree(input: string[]): void {
  * @param parentDirectory - Parent directory of the newly created directory
  */
 function createNewDirectory(directoryName: string, parentDirectory: dir): void {
+
+  // Check if the given parent directory already has a child directory with the given name
   const childWithNameExists = parentDirectory.children.find(d => d.name === directoryName);
   if (childWithNameExists !== undefined) {
-    // We should not add the same child directory again
+    // We should not try add the same child directory again
+    // This could happen as the `createNewDirectory` is triggered each time we encounter this directory during an `ls`
     return;
   }
+
+  // Create the new directory
   const directory: dir = {
     name: directoryName,
-    // The root dir doesn't have a parent
     parent: parentDirectory,
     children: [],
     files: [],
     size: 0
   };
+
   // Add the new dir as a child to the parent dir
-  parentDirectory.children?.push(directory);
+  parentDirectory.children.push(directory);
 }
 
 /**
- * Update the size of the directory, and it's parent (recursively)
- * TODO:  This is not the most efficient as for each file we traverse up the child-parent hierarchy
- *        We could achieve the same thing quicker by adding up the child directory sizes when we traverse the tree.
+ * Update the size of the directory, and it's parents recursively
+ * TODO:  This is not the most efficient, as for each file we traverse up the child-parent hierarchy
+ *        We could achieve the same thing quicker by adding up the child directory sizes when we traverse the final tree.
  * @param directory - The directory we want to update
  * @param size - The size of the file
  */
-function addFilesSize(directory: dir, size: number): void {
+function updateDirectorySizeWithFileSize(directory: dir, size: number): void {
   directory.size += size;
   if (directory.parent !== undefined) {
-    addFilesSize(directory.parent, size);
+    updateDirectorySizeWithFileSize(directory.parent, size);
   }
-
 }
 
 /**
@@ -164,6 +165,7 @@ function traverseDirectoryTree(): void {
  * @param directories 
  */
 function traverseChildDirectories(directories: dir[]): void {
+
   for (const directory of directories) {
     // Loop over all directories
 
