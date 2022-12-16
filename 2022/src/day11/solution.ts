0e4d58ec975f04d22311cde7ecd4531cc31930d7
Istvan Csanyi <istvan.csanyi@onlogic.com>
2022-12-16 10:10:31 +0100

    [2022 Day11] Use LCD

--- 2022/src/day11/solution.ts
+++ 2022/src/day11/solution.ts
@@ -1,6 +1,6 @@
 import { performance } from 'perf_hooks';
 
-// Set up Monkey type
+// The Monkey
 type Monkey = {
   idx: number,
   activity: number,
@@ -12,9 +12,6 @@ type Monkey = {
   falseMonkeyIdx: number;
 };
 
-// Main array we use to keep track of the Monkeys
-const monkeys: Monkey[] = [];
-
 /**
  * Main function to trigger all functionality needed to solve the daily challenge
  * @param input - `raw` string content of the input file
@@ -22,37 +19,16 @@ const monkeys: Monkey[] = [];
 export async function solve(input: string, dayNumber: string, usingExample: boolean): Promise<void> {
   const st = performance.now();
   console.log(`---- Day ${dayNumber} ----`);
-
-
-  // Pre-process the data to populate the `monkeys` array
-  for (const block of input.split("Monkey ").filter(x => x !== "")) {
-    const data = block.split("\n");
-    monkeys.push({
-      idx: +data[0].replace(":", ""),
-      activity: 0,
-      items: data[1].replace("Starting items: ", "").split(",").map(x => +x.trim()),
-      operation: data[2].split("new = ")[1].split(" ")[1],
-      operationOn: data[2].split("new = ")[1].split(" ")[2],
-      divisibleBy: +data[3].replace("Test: divisible by ", ""),
-      trueMonkeyIdx: +data[4].split("throw to monkey ")[1],
-      falseMonkeyIdx: +data[5].split("throw to monkey ")[1]
-    });
-  }
-
-  [2].forEach(part => doIt(part));
+  [1, 2].forEach(part => doIt(input, part));
   console.log(`Done in ${((performance.now() - st) / 1000).toFixed(4)}s`);
 }
 
-function doIt(part: number): void {
+function doIt(input: string, part: number): void {
+  const [monkeys, lcd] = getMonkeyData(input);
   const rounds = part === 1 ? 20 : 10000;
-  // Play the 20 rounds
-  for (let i = 1; i < rounds+1; i++) {
-    playRound(part);
-    if ([1, 20, 1000].includes(i)) {
-      console.log(`Round ${i}`);
-      monkeys.forEach(monkey => console.log(`Monkey ${monkey.idx}: ${monkey.activity}`));
-      console.log('');
-    }
+  // Play the round for the part
+  for (let i = 1; i < rounds + 1; i++) {
+    playRound(monkeys, lcd, part);
   }
   const result = monkeys.map(x => x.activity).sort((a, b) => b - a).slice(0, 2).reduce(function (product, value) { return product * value; });
   console.log(`Part${1}: ${result}`);
@@ -61,44 +37,76 @@ function doIt(part: number): void {
 /**
  * Play the round where Monkeys swap items between each other
  */
-function playRound(part: number): void {
+function playRound(monkeys: Monkey[], lcd: number, part: number): void {
   // Loop through each monkey
   monkeys.forEach(monkey => {
-    const monkeyItems = [...monkey.items];
-    monkey.items = [];
-    // Empty out the list of items for the Monkey
-    // we do this as we don't want to alter the list while we iterate over it
-    // TODO: I bet there is a better way to do this in JS... find it...
-
     // Loop through each item at the monkey
-    while (monkeyItems.length) {
+    while (monkey.items.length > 0) {
+
       // Remove the element from the monkey
-      const [item] = monkeyItems.splice(0, 1);
-      const originalValue = item
+      const [item] = monkey.items.splice(0, 1);
+
+      // Update the Monkey activity
       monkey.activity++;
+
       // Calculate the Worry Level
-      let baseWorryLevel: number;
-      if (monkey.operation === "*") {
-        baseWorryLevel = item * (monkey.operationOn === "old" ? item : +monkey.operationOn);
-      } else {
-        baseWorryLevel = item + (monkey.operationOn === "old" ? item : +monkey.operationOn);
-      }
+      const value = (monkey.operationOn === "old" ? item : +monkey.operationOn);
+      const baseWorryLevel = monkey.operation === "*" ? item * value : item + value;
 
       let worryLevel: number;
       if (part === 1) {
         worryLevel = Math.floor(baseWorryLevel / 3);
       } else {
-        worryLevel = baseWorryLevel;
+        /**
+         * Use `Least Common Denominator` to reduce the number
+         * Ex. Check division for 3, 7 for number 2187:
+         *      lcd = 21 (3*7)
+         *      baseWorryLevel = 27
+         *      2187 / 21 = 104,142857143 => Math.floor(2187 / 21) = 104
+         *      104 * lcd = 2184
+         *      2187 - 2184 = 3
+         *      At this point we have "split" the original number (the number we want to "reduce") to 2184 and 3
+         *      We know that `2184` is divisible with both 7 and 3, which means we only need to investigate the divisibility of the remainder
+         *      3 % 3 === 0 will be the same as 2187 % 3 === 0
+         *      3 % 7 === 0 will be the same as 2187 % 7 === 0
+         *      This means we can pass the remainder along as a new item for the next Monkey, instead of the ever growing number
+         */
+        worryLevel = baseWorryLevel > lcd ? baseWorryLevel - (Math.floor(baseWorryLevel / lcd) * lcd) : baseWorryLevel;
       }
 
       // Check the rule and pass to the correct other monkey
       if (worryLevel % monkey.divisibleBy === 0) {
-        console.log(`Monkey ${monkey.idx} | ${originalValue} -> ${worryLevel} => Monkey ${monkey.trueMonkeyIdx}`);
         monkeys[monkey.trueMonkeyIdx].items.push(worryLevel);
       } else {
-        console.log(`Monkey ${monkey.idx} | ${originalValue} -> ${worryLevel} => Monkey ${monkey.falseMonkeyIdx}`);
         monkeys[monkey.falseMonkeyIdx].items.push(worryLevel);
       }
+
     };
   });
+}
+
+/**
+ * Get the Monkey data by parsing the input
+ * @param input - The Input
+ */
+function getMonkeyData(input: string): [Monkey[], number] {
+  let lcd = 1;
+  let monkeys: Monkey[] = [];
+  // Pre-process the data to populate the `monkeys` array
+  for (const block of input.split("Monkey ").filter(x => x !== "")) {
+    const data = block.split("\n");
+    const divisible = +data[3].replace("Test: divisible by ", "");
+    monkeys.push({
+      idx: +data[0].replace(":", ""),
+      activity: 0,
+      items: data[1].replace("Starting items: ", "").split(",").map(x => +x.trim()),
+      operation: data[2].split("new = ")[1].split(" ")[1],
+      operationOn: data[2].split("new = ")[1].split(" ")[2],
+      divisibleBy: divisible,
+      trueMonkeyIdx: +data[4].split("throw to monkey ")[1],
+      falseMonkeyIdx: +data[5].split("throw to monkey ")[1]
+    });
+    lcd *= divisible;
+  }
+  return [monkeys, lcd];
 }
